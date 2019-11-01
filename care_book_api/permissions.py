e9dad890c0deb79cdbc035c3ee51a948ab8fb81e
Mariam <Mariamhh98@hotmail.com>
2019-11-01 18:12:54 +0300

    for shereen 2

--- care_book_api/permissions.py
+++ care_book_api/permissions.py
@@ -1,1 +1,10 @@
 from rest_framework.permissions import BasePermission
+
+class IsParent(BasePermission):
+	message = "You must be a parent of the home!"
+
+	def has_object_permission(self, request, view, obj):
+		if (obj.user == request.user):
+			return True
+		else:
+			return False
