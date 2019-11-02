7019a898ae5183fa4a9f0a23c520b5b0e2518838
shereengh <shereengaa@gmail.com>
2019-11-02 18:43:14 +0300

    home list

--- care_book/urls.py
+++ care_book/urls.py
@@ -8,14 +8,16 @@ from django.conf.urls.static import static
 from care_book_api import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 
-    path('add/child/<int:home_id>/', views.AddChild.as_view(), name='add_child'),
+    path("add/child/<int:home_id>/", views.AddChild.as_view(), name="add_child"),
 
     path("invite/<int:home_id>/", views.UserInvite.as_view(), name="invite"),
 
     path("signup/", views.Signup.as_view(), name="signup"),
-    path('login/', views.MyTokenObtainPairView.as_view(), name='login'), 
+    path("login/", views.MyTokenObtainPairView.as_view(), name="login"), 
+
+    path("homes/",views.HomeList.as_view(), name= "homes"),
 ]
 
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
